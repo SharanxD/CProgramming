a3fa55ee54354ec4847a0c22bb0500d1415275d1
SharanxD <100334111+SharanxD@users.noreply.github.com>
2022-03-10 09:04:28 +0530

    Simple Program of Concatenating two Strings

--- stringfunctions.c
+++ stringfunctions.c
@@ -1,15 +1,12 @@
 #include<stdio.h>  
 #include <string.h>    
 int main(){    
-    char ch1[100],ch22[100];
+    char ch1[100],ch2[100];
     printf("Enter string 1: ");
     scanf("%s",&ch1);
     printf("Enter string 2: ");
-    scanf("%s",&ch22);
-    //char ch[100]="Hello ";    
-    //char ch2[100]="C Programming";    
-    //strcat(ch,ch2);
-    strcat(ch1,ch22);
+    scanf("%s",&ch2);
+    strcat(ch1,ch2);
     printf("Value of string 1, after concatenation is: %s",ch1);    
     return 0;    
 }    
