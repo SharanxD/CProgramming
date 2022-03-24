858552f395ad39bb27b033c7ad15eb3af48f6673
Sharan P <100334111+SharanxD@users.noreply.github.com>
2022-03-24 10:45:59 +0530

    Module Program

--- 2.cpp
+++ 2.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
-void mod(int a,int b){
-	if(a%b==0) printf("Perfect Module");
+void mod(float a,float b){
+	if((int)a%(int)b==0) printf("Perfect Module");
 	else printf("Not a perfect module");
 }
 int main(){
-	int a,b;
-	scanf("%d %d",&a,&b);
+	float a,b;
+	scanf("%f %f",&a,&b);
 	mod(a,b);
 }
